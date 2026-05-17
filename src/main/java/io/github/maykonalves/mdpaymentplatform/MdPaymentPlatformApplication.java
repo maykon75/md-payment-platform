f7518c1a6fb78f8b945c5df3808d7ae594606552
maykon75 <maykonmda2002@gmail.com>
2026-05-17 09:44:38 -0300

    Init

---
+++ src/main/java/io/github/maykonalves/mdpaymentplatform/MdPaymentPlatformApplication.java
@@ -0,0 +1,13 @@
+package io.github.maykonalves.mdpaymentplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdPaymentPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MdPaymentPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/maykonalves/mdpaymentplatform/MdPaymentPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.maykonalves.mdpaymentplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdPaymentPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
